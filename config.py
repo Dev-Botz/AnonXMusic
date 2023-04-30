c185dbb9b257ee22c3cc297264c5f31a4bb1d648
MOCA <116737841+Moca96@users.noreply.github.com>
2023-05-01 02:27:38 +0530

    Update config.py
    Signed-off-by: MOCA <116737841+Moca96@users.noreply.github.com>

--- config.py
+++ config.py
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/56d1760224589ee370186.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/209fd30876b3184dfef55.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/56d1760224589ee370186.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/209fd30876b3184dfef55.jpg"
